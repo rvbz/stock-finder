cd75b3dd4fa54cb4abd12048bc2a4e508defd270
Ruben Ruiz <rvbz@hotmail.com>
2022-10-10 19:54:51 +0100

    Created first Laravel app for managing stocks with facebook login included

---
+++ config/services.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+        'scheme' => 'https',
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    'facebook' => [
+        'client_id' => '408962648093794', //USE FROM FACEBOOK DEVELOPER ACCOUNT
+        'client_secret' => '05fcdf7f72a7687b6d79ca57cd76ecfa', //USE FROM FACEBOOK DEVELOPER ACCOUNT
+        'redirect' => 'http://localhost/facebook/callback/'
+    ],
+
+];
